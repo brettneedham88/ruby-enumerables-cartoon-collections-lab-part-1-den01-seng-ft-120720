2e560d9f4f3cb066d512a1a4097762ccb85992f4
Brett Needham <brettneedham88@gmail.com>
2020-11-20 18:29:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-array.each.with_index(1) do |index, name|
+array.each.with_index(1) do |name, index|
   puts"#{index}. #{name}"
  end
 end
